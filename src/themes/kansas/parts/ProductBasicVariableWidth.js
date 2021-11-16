5ee70fedabe681f58b6553bc619ff515992c631f
Kieren <kieren@carderbee.com>
2021-11-16 12:04:50 +0000

    add option to view product details, update favicon for ukcarderbeecom

--- src/themes/kansas/parts/ProductBasicVariableWidth.js
+++ src/themes/kansas/parts/ProductBasicVariableWidth.js
@@ -21,29 +21,33 @@ export default function ProductBasicVariableWidth(props) {
                         null
                 }
                 <a href={props.offer.product._source.click_out_url}>
-                    <div title={props.offer.product._source.title} className='image' style={{
-                        background: `url(${props.offer.product._source.image_large})`
-                    }}></div>
+                    <div className='visit-store'>
+                        <p>Visit Store <i className="fas fa-external-link-alt"></i></p>
+                    </div>
                 </a>
+                <div title={props.offer.product._source.title} className='image' style={{
+                    background: `url(${props.offer.product._source.image_large})`
+                }}></div>
                 <div className='product-text'>
-                    <a href={props.offer.product._source.click_out_url}>
-                        <h5 className='d-none d-md-block' title={props.offer.product._source.title}>{props.offer.product._source.title}</h5>
-                        <h6 className='d-md-none' title={props.offer.product._source.title}>{props.offer.product._source.title}</h6>
-                        <hr/>
-                        <p className='price'>
-                            {new Intl.NumberFormat(props.siteConfig.locale, { style: 'currency', currency: props.siteConfig.currency }).format(props.offer.product._source.price)}
-                        </p>
-                        <p className='merchant'>{props.offer.product._source.merchant}</p>
-                        <p>
-                        {
-                            parseFloat(props.offer.product._source.delivery_cost) === 0 ?
-                                <span title={props.offer.product._source.delivery_time ? props.offer.product._source.delivery_time : null} className='shipping free'>Free delivery</span> :
-                                <span title={props.offer.product._source.delivery_time ? props.offer.product._source.delivery_time : null} className='shipping'>{new Intl.NumberFormat(props.siteConfig.locale, { style: 'currency', currency: props.siteConfig.currency }).format(props.offer.product._source.delivery_cost)} delivery</span>
-                        }
-                        </p>
-                    </a>
+                    <h5 className='d-none d-md-block' title={props.offer.product._source.title}>{props.offer.product._source.title}</h5>
+                    <h6 className='d-md-none' title={props.offer.product._source.title}>{props.offer.product._source.title}</h6>
+                    <hr/>
+                    <p className='price'>
+                        {new Intl.NumberFormat(props.siteConfig.locale, { style: 'currency', currency: props.siteConfig.currency }).format(props.offer.product._source.price)}
+                    </p>
+                    <p className='merchant'>{props.offer.product._source.merchant}</p>
+                    <p>
+                    {
+                        parseFloat(props.offer.product._source.delivery_cost) === 0 ?
+                            <span title={props.offer.product._source.delivery_time ? props.offer.product._source.delivery_time : null} className='shipping free'>Free delivery</span> :
+                            <span title={props.offer.product._source.delivery_time ? props.offer.product._source.delivery_time : null} className='shipping'>{new Intl.NumberFormat(props.siteConfig.locale, { style: 'currency', currency: props.siteConfig.currency }).format(props.offer.product._source.delivery_cost)} delivery</span>
+                    }
+                    </p>
                     <a href={`/shopping/product/${base64_encode(props.offer.product._source.product_code)}`}>
-                        {props.offer.offer_count > 1 && showOfferCount ? <Fragment><hr/><p className='compare-link'>Compare from {props.offer.offer_count} offers</p></Fragment> : null}
+                        {props.offer.offer_count > 1 && showOfferCount ?
+                            <Fragment><hr/><p className='compare-link'>Compare from {props.offer.offer_count} offers</p></Fragment> :
+                            <Fragment><hr/><p className='compare-link'>View offer details</p></Fragment>
+                        }
                     </a>
                 </div>
             </div>
